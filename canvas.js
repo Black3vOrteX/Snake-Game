eacdbac6428af94a15d488bb6abe2f13c113fc90
Black3vOrteX <130942494+Black3vOrteX@users.noreply.github.com>
2026-02-23 18:54:46 +0530

    Add debug borders to inspect layout

---
+++ canvas.js
@@ -0,0 +1,5 @@
+var canvas = document.getElementById("game canvas");
+var header = document.getElementById("header");
+canvas.width=header.offsetWidth;
+canvas.height=window.innerHeight;
+console.log(canvas);
