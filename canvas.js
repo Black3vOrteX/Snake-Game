a88bdac0fb5dd8dc7c4d0624b52a12a195f5f27f
Black3vOrteX <130942494+Black3vOrteX@users.noreply.github.com>
2026-02-23 20:58:58 +0530

    created lines

--- canvas.js
+++ canvas.js
@@ -1,5 +1,23 @@
-var canvas = document.getElementById("game canvas");
+var canvas = document.getElementById("gameCanvas");
 var header = document.getElementById("header");
-canvas.width=header.offsetWidth;
-canvas.height=window.innerHeight;
+
+function resizeCanvas(){
+    var width = header.getBoundingClientRect().width;
+    canvas.width=width;
+    canvas.style.width=width + "px";
+    canvas.height=window.innerHeight;
+}
+resizeCanvas();
+
+var c = canvas.getContext("2d");
+c.fillStyle = "blue";
+c.fillRect(50,50,50,50);
+
+c.moveTo(150,250);
+c.lineTo(250,250);
+c.lineTo(200,150);
+c.closePath();
+c.strokeStyle = "pink";
+c.stroke();
+
 console.log(canvas);
