1de728c82c170360dbffc410467ee2003460602a
Black3vOrteX <130942494+Black3vOrteX@users.noreply.github.com>
2026-02-24 20:17:18 +0530

    snake moves

--- canvas.js
+++ canvas.js
@@ -29,7 +29,43 @@ let snakeRow = 10;
 c.fillStyle = "pink";
 c.fillRect(snakeCol*unit,snakeRow*unit,unit,unit);
 
+let dx = 0;
+let dy = 0;
+document.addEventListener("keydown",function(e){
 
+    if(e.key === "ArrowUp"){
+        dx = 0;
+        dy = -1;
+    }
+    else if(e.key === "ArrowDown"){
+        dx = 0;
+        dy = 1;
+    }
+    else if(e.key === "ArrowLeft"){
+        dx = -1;
+        dy = 0;
+    }
+    else if(e.key === "ArrowRight"){
+        dx = 1;
+        dy = 0;
+    }
+});
+
+function gameloop(){
+    c.clearRect(0,0,canvas.width,canvas.height);
+    for(let row = 0; row < 20; row ++){
+        for(let col = 0; col < 20; col++){
+            c.strokeStyle = "white";
+            c.strokeRect(col*unit,row*unit,unit,unit);
+        }
+    }
+
+snakeCol += dx;
+snakeRow += dy;
+c.fillStyle = "pink";
+c.fillRect(snakeCol*unit,snakeRow*unit,unit,unit);
+}
+setInterval(gameloop,100);
 
 
 
