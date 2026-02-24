078899894faa575a8a55e99698b46eba219e589c
Black3vOrteX <130942494+Black3vOrteX@users.noreply.github.com>
2026-02-24 21:37:50 +0530

    collision detection

--- canvas.js
+++ canvas.js
@@ -4,9 +4,6 @@ const size = 600;
 canvas.width=size;
 canvas.height=size;
 
-
-
-
 var c = canvas.getContext("2d");
 
 c.fillStyle = "pink";
@@ -14,16 +11,11 @@ c.strokeStyle = "black";
 
 const unit = 30;
 
-
-
 let snakeCol = 10;
 let snakeRow = 10;
 
-
-
-
-let dx = 0;
-let dy = 0;
+let dx = 0; //change in x axis
+let dy = 0; //change in y axis
 document.addEventListener("keydown",function(e){
 
     if(e.key === "ArrowUp"){
@@ -50,6 +42,12 @@ function gameloop(){
         for(let col = 0; col < 20; col++){
             c.strokeStyle = "black";
             c.strokeRect(col*unit,row*unit,unit,unit);
+            if(snakeCol < 0 || snakeCol > 19 || snakeRow < 0 || snakeRow > 19){
+    alert("Game Over");
+    snakeCol = 10;
+    snakeRow = 10;
+}
+
         }
     }
 
@@ -58,12 +56,11 @@ snakeRow += dy;
 c.fillStyle = "pink";
 c.fillRect(snakeCol*unit,snakeRow*unit,unit,unit);
 }
-setInterval(gameloop,100);
-
-
-
-
-
-
+setInterval(gameloop,80);
+if(snakeCol < 0 || snakeCol > 19 || snakeRow < 0 || snakeRow > 19){
+    alert("Game Over");
+    snakeCol = 10;
+    snakeRow = 10;
+}
 
 console.log(canvas);
