2b447b9b97110c454e585dfaaeadb2b89a02180b
Black3vOrteX <130942494+Black3vOrteX@users.noreply.github.com>
2026-02-24 15:00:09 +0530

    save progress

--- canvas.js
+++ canvas.js
@@ -10,29 +10,18 @@ function resizeCanvas(){
 }
 resizeCanvas();
 
-var c = canvas.getContext("2d");
-c.fillStyle = "blue";
-c.fillRect(50,50,50,50);
-
-c.moveTo(150,250);
-c.lineTo(250,250);
-c.lineTo(200,150);
-c.closePath();
-c.strokeStyle = "pink";
-c.stroke();
-var canvas = document.getElementById("gameCanvas");
-var header = document.getElementById("header");
-
-function resizeCanvas(){
-    var width = header.getBoundingClientRect().width;
-    canvas.width=width;
-    canvas.style.width=width + "px";
-    canvas.height=window.innerHeight;
-}
-resizeCanvas();
+canvas.width=900;
 
 var c = canvas.getContext("2d");
-c.fillStyle = "lightseagreen";
-c.fillRect(50,50,50,50);
+c.fillStyle = "pink";
+c.fillRect(50,50,30,30);
+c.fillRect(100,50,30,30);
+c.fillRect(150,50,30,30);
+c.fillRect(200,50,30,30);
+
+
+
+
+
 
 console.log(canvas);
