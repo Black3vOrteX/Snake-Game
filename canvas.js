d1ac6cf5ddda8c901567c866104aa5e082961f76
Black3vOrteX <130942494+Black3vOrteX@users.noreply.github.com>
2026-02-24 19:10:48 +0530

    snake position at center

--- canvas.js
+++ canvas.js
@@ -10,18 +10,24 @@ canvas.height=size;
 var c = canvas.getContext("2d");
 
 c.fillStyle = "pink";
-c.strokeStyle = "black";
+c.strokeStyle = "white";
 
 const unit = 30;
 
 for(let row=0;row<20;row++){
     for(let col=0;col<20;col++)
         {
-         c.strokeStyle = "black";
+       
         // c.fillRect(col*unit,row*unit,unit,unit);
         c.strokeRect(col*unit,row*unit,unit,unit);
     }
 }
+
+let snakeCol = 10;
+let snakeRow = 10;
+
+c.fillStyle = "pink";
+c.fillRect(snakeCol*unit,snakeRow*unit,unit,unit);
 
 
 
